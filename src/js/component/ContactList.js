ef4d15c7941a8e241a2fe0573fd8ec1d1cb3d91b
Eunice Gallardo <128846071+Eunicegallardo@users.noreply.github.com>
2023-07-29 17:05:07 +0000

    Make the card and finish the design

--- src/js/component/ContactList.js
+++ src/js/component/ContactList.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { FaTrashAlt } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhone} from "react-icons/fa";
+import { FaEnvelope} from "react-icons/fa";
+
 
 const ContactList = () => {
     return (
         <div>
-            <div className="d-flex justify-content-end">
-                <Link to="/form">
-                    <button className="btn">Check the Context in action</button>
-                </Link>
-            </div>
+
             <div className='container'>
-                <div className="card mb-3" style={{maxWidth: "540px"}}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" className="img-fluid rounded-start" alt="..."/>
+                <div className="card border-success mb-3" >
+                    <div className="row">
+                        <div className="col-3">
+                            <img src="https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" className="img-fluid my-4 ms-5" alt="..." />
                         </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        <div className="col-7">
+                            <div className="card-body ps-4 pt-4">
+                                <h5 className="card-title m-3">Emily Sandoval</h5>
+                                <p className="card-text"><FaMapMarkerAlt className='m-2'/> 5842 Hillcrest Rd</p>
+                                <p className="card-text"><FaPhone className='m-2'/>(870) 288-4149 </p>
+                                <p className="card-text"><FaEnvelope className='m-2'/> sandovalemily@gmail.com</p>
                             </div>
+                        </div>
+                        <div className='col-2 pt-5'>
+                            <FaEdit className='m-5'></FaEdit>
+                            <FaTrashAlt></FaTrashAlt>
                         </div>
                     </div>
                 </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,9 +3,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import FormContact from "../component/FormContact";
 import ContactList from "../component/ContactList";
+import { Link } from "react-router-dom";
+
 
 export const Home = () => (
 	<div className="container mt-5">
+		<div className="d-flex justify-content-end m-4">
+                <Link to="/form">
+                    <button className="btn">Add a new contact</button>
+                </Link>
+            </div>
 		<ContactList></ContactList>
 	</div>
 );
